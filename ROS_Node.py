Bug fix in traffic light detection and stop sign detection
 Changes to be committed:
	modified:   ROS_Node.py
	modified:   main_carla.py

ROS_Node.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion, Twist, PoseW
 from nav_msgs.msg import Path, Odometry
 from sensor_msgs.msg import PointCloud2, PointField
 from dbw_mkz_msgs.msg import ThrottleCmd,BrakeCmd,SteeringCmd
-from std_msgs.msg import UInt8
+from std_msgs.msg import Int8, Time
 from tf.transformations import quaternion_from_euler
 
 import carla
@@ -30,12 +30,12 @@ class ROS_Node(object):
         self.stop_list	= self.actor_list.filter("*stop*")
         self.car_list	= self.actor_list.filter("*vehicle*")
         self.ped_list	= self.actor_list.filter("*walker*")
+        self.action     = -1 
 
         rospy.init_node('{}_ros_node'.format(self.ns), anonymous = True)
         rospy.Subscriber('{}/ThrottleCmd'.format(self.ns), ThrottleCmd, self.callback_throttle)
         rospy.Subscriber('{}/BrakeCmd'.format(self.ns), BrakeCmd, self.callback_brake)
         rospy.Subscriber('{}/SteeringCmd'.format(self.ns), SteeringCmd, self.callback_steering)
-
         rospy.Subscriber('{}/action'.format(self.ns), Int8, self.callback_action)
         # self.callback_throttle(ThrottleCmd())
         self.path_publisher = rospy.Publisher('{}/global_path'.format(self.ns), Path, queue_size = 10)
@@ -44,7 +44,7 @@ class ROS_Node(object):
         self.LiDAR_publisher = rospy.Publisher('{}/LiDAR'.format(self.ns), PointCloud2, queue_size = 10)
         self.left_lanemarking_publisher = rospy.Publisher('{}/left_lanemarking'.format(self.ns), Path, queue_size = 10)
         self.right_lanemarking_publisher = rospy.Publisher('{}/right_lanemarking'.format(self.ns), Path, queue_size = 10)
-        self.heading_traffic_light_publisher = rospy.Publisher('{}/heading_traffic_light'.format(self.ns), UInt8, queue_size = 10)
+        self.heading_traffic_light_publisher = rospy.Publisher('{}/heading_traffic_light'.format(self.ns), Int8, queue_size = 10)
         self.heading_traffic_light_publisher = rospy.Publisher('{}/heading_traffic_light'.format(self.ns), Int8, queue_size = 10)
         self.heading_stop_sign_publisher = rospy.Publisher('{}/heading_stop_sign'.format(self.ns), Int8, queue_size = 10)
         self.wp_state_publisher = rospy.Publisher('{}/wp_state'.format(self.ns), Int8, queue_size = 10)
@@ -162,9 +162,8 @@ class ROS_Node(object):
         
     def publish_route(self):
         waypoints_in_costmap = []
-        for wp in self.waypoints:
         count_wp_in_front = 0
-            if (self.is_within_distance_ahead(wp.transform,self.stage.player.get_transform(),50)):
+        for wp in self.waypoints:
             if not count_wp_in_front:
                 if (self.is_within_distance_ahead(wp.transform,self.stage.player.get_transform(),10)):
                     waypoints_in_costmap.append(wp)
@@ -177,7 +176,6 @@ class ROS_Node(object):
 
         for idx in range(0,len(waypoints_in_costmap),10):
             wp = waypoints_in_costmap[idx]
-            self.stage.world.debug.draw_point(wp.transform.location, color=carla.Color(0, 255, 0), life_time=0.2)
             if wp.is_intersection and self.action == 7:
                 color1 = carla.Color(255, 0, 0)
                 color2 = carla.Color(255, 0, 0)
@@ -188,9 +186,9 @@ class ROS_Node(object):
                 color1 = carla.Color(0, 255, 0)
                 color2 = carla.Color(0, 0, 255)
             self.stage.world.debug.draw_point(wp.transform.location, color=color1, life_time=0.2)
-            self.stage.world.debug.draw_point(left_marking, color=carla.Color(255, 0, 0), life_time=0.2)
+            left_marking = self.lateral_shift(wp.transform, -wp.lane_width * 0.5)
             self.stage.world.debug.draw_point(left_marking, color=color2, life_time=0.2)
-            self.stage.world.debug.draw_point(right_marking, color=carla.Color(255, 0, 0), life_time=0.2) 
+            right_marking = self.lateral_shift(wp.transform, wp.lane_width * 0.5)
             self.stage.world.debug.draw_point(right_marking, color=color2, life_time=0.2) 
         left_lanemarking_msg = Path()
         right_lanemarking_msg = Path()
@@ -253,19 +251,17 @@ class ROS_Node(object):
         distance = np.linalg.norm(np.array([loc1.x-loc2.x,loc1.y-loc2.y,loc1.z-loc2.z]))
 
 
-        if distance < 0.1:
         if distance < 10:
             return False
         elif distance > max_distance: # if the light is close to the car
             return False
 
         else:
-            yaw3 = np.arctan2(loc1.y-loc2.y,loc1.x-loc2.x)
-            d_angle1 = abs((yaw3-yaw2+180)%360-180) < 20.0 # if the light is in front of the car
-            d_angle1 = abs((yaw3-yaw2+180)%360-180) < 45.0 # if the light is in front of the car
+            yaw3 = np.degrees(np.arctan2(loc1.y-loc2.y,loc1.x-loc2.x))
+            d_angle1 = abs(abs((yaw3-yaw2+180)%360-180)-0)  < 20.0 # if the light is in front of the car
+            d_angle2 = abs(abs((yaw1-yaw2+180)%360-180)-90) < 20.0 # if the light is facing to the car
+            # print(d_angle1 , d_angle2)
             return  d_angle1 and d_angle2
-            # return  d_angle1 and d_angle2
-            return d_angle2
 
     def detect_light(self):
         tf_ego = self.stage.player.get_transform()
@@ -273,9 +269,9 @@ class ROS_Node(object):
         for light in self.light_list:
             if self.is_target_light(light.get_transform(),tf_ego,60):
                 heading_traffic_light = light
-                break
                 # print(light.get_transform())
                 # print(light.state)
+                break
         
         state = 0
         if not heading_traffic_light:
@@ -307,35 +303,32 @@ class ROS_Node(object):
             return False
 
         else:
-            # return True
-            yaw3 = np.arctan2(loc1.y-loc2.y,loc1.x-loc2.x)
+            yaw3 = np.degrees(np.arctan2(loc1.y-loc2.y,loc1.x-loc2.x))
             wp_stop_sign  = self.stage.map.get_waypoint(loc1)
             wp_ego_car    = self.stage.map.get_waypoint(loc2)
-            print(wp_stop_sign.road_id,wp_ego_car.road_id)
-            print(wp_stop_sign.lane_id,wp_ego_car.lane_id)
-            d_angle1 = abs((yaw3-yaw2+180)%360-180) < 20.0 # if the light is in front of the car
-            d_angle1 = abs((yaw3-yaw2+180)%360-180) < 90.0 # if the light is in front of the car
-            # d_angle2 = abs((yaw1-yaw2+180)%360-180-180) < 20.0 # if the light is facing to the car
-            return  d_angle1 #and d_angle2
-        
+            # print(wp_stop_sign.road_id,wp_ego_car.road_id)
+            d_angle1 = abs(abs((yaw3-yaw2+180)%360-180)-0) #< 20.0 # if the light is in front of the car
+            d_angle2 = abs(abs((yaw1-yaw2+180)%360-180)-180) #< 20.0 # if the light is facing to the car
+            # print(yaw3,yaw2)
+            # print(d_angle1,d_angle2)
+            # return True
+            return  d_angle1 and d_angle2
+            
     def detect_stop(self):
         tf_ego = self.stage.player.get_transform()
-        # tf_ego = self.stage.player.get_transform()
-        # heading_stop_sign = None
-        # for stop in self.stop_list:
-        #     if self.is_target_stop(stop.get_transform(),tf_ego,60):
-        #         heading_stop_sign = stop
-        #         # print(stop.get_transform())
+        heading_stop_sign = None
+        for stop in self.stop_list:
+            if self.is_target_stop(stop.get_transform(),tf_ego,30):
+                heading_stop_sign = stop
+                # print(stop.get_transform())
 
         state = 0
-        # state = 0
-        # if not heading_stop_sign:
-        #     state = 0
-        # else:
-        #     state = 1
+        if not heading_stop_sign:
+            state = 0
+        else:
+            state = 1
         self.heading_stop_sign_publisher.publish(state)
-        # self.heading_stop_sign_publisher.publish(state)
-        self.heading_stop_sign_publisher.publish(0)
+
     def publish_clock(self):
         time = self.stage.hud.simulation_time
         time_msg = rospy.Time.from_sec(time)